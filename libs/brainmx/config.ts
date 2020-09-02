0c38f5a02204b2304879b19cfc2b52c66d282964
gfdfranco <gfd.francodelgado@gmail.com>
2020-09-02 10:59:30 -0500

    BrainMX PINS changed

--- libs/brainmx/config.ts
+++ libs/brainmx/config.ts
@@ -2,26 +2,14 @@ namespace config {
     export const PIN_NEOPIXEL = DAL.PA21;
     export const NUM_NEOPIXELS = 3;
     export const PIN_LED = DAL.PA17;
-    export const PIN_LIGHT = DAL.PA21;
-    export const PIN_A0 = DAL.PA02;
-    export const PIN_A1 = DAL.PB08;
-    export const PIN_A2 = DAL.PB09;
-    export const PIN_D6 = DAL.PA20;
-    export const PIN_D8 = DAL.PA06;
-    export const PIN_D10 = DAL.PA18;
-    export const PIN_D11 = DAL.PA16;
-    export const PIN_D12 = DAL.PA19;
-    export const PIN_D13 = DAL.PA17;
 
-    export const PIN_D17 = DAL.PA21;
-        
-    export const PIN_D0 = DAL.PA11;
-    export const PIN_D1 = DAL.PA10;
-    export const PIN_D9 = DAL.PA15;
+    export const PIN_D5 = DAL.PB08;
+    export const PIN_D8 = DAL.PB09;
+    export const PIN_A0 = DAL.PA20;
+    export const PIN_A1 = DAL.PA06;
+    export const PIN_A2 = DAL.PA18;
+    export const PIN_A3 = DAL.PA16;
 
-    export const PIN_D7 = DAL.PA07;
-    export const PIN_SPEAKER_AMP = DAL.PA07;
-    export const PIN_MICROPHONE = DAL.PA07;
-    // additional pins to look for: SPI, I2C, Flash
+    export const PIN_D4 = DAL.PA07;
 }
 
--- libs/brainmx/device.d.ts
+++ libs/brainmx/device.d.ts
@@ -1,40 +1,23 @@
 declare namespace pins {
     //% fixedInstance shim=pxt::getPin(PIN_LED)
     const LED: PwmOnlyPin;
-    //% fixedInstance shim=pxt::getPin(PIN_A0)
-    const ENC: PwmPin;
-    //% fixedInstance shim=pxt::getPin(PIN_A1)
-    const ENB: PwmPin;
-    //% fixedInstance shim=pxt::getPin(PIN_A2)
-    const ENA: PwmPin;
-    //% fixedInstance shim=pxt::getPin(PIN_D6)
-    const MA1: PwmPin;
+    //% fixedInstance shim=pxt::getPin(PIN_D5)
+    const ENABLE: DigitalInOutPin;
     //% fixedInstance shim=pxt::getPin(PIN_D8)
+    const ENABLE_V1: DigitalInOutPin;
+    //% fixedInstance shim=pxt::getPin(PIN_A0)
+    const MA1: PwmPin;
+    //% fixedInstance shim=pxt::getPin(PIN_A1)
     const MA2: PwmPin;
-    //% fixedInstance shim=pxt::getPin(PIN_D10)
-    const MB2: PwmPin;
-    //% fixedInstance shim=pxt::getPin(PIN_D11)
+    //% fixedInstance shim=pxt::getPin(PIN_A3)
     const MB1: PwmPin;
-    //% fixedInstance shim=pxt::getPin(PIN_D12)
-    const MC1: PwmPin;
-    //% fixedInstance shim=pxt::getPin(PIN_D13)
-    const MC2: PwmPin;
+    //% fixedInstance shim=pxt::getPin(PIN_A2)
+    const MB2: PwmPin;
     
-    //% fixedInstance shim=pxt::getPin(PIN_D7)
-    const SOUND: PwmPin;
-
-    //% fixedInstance shim=pxt::getPin(PIN_D0)
-    const DIN: PwmPin;
-    //% fixedInstance shim=pxt::getPin(PIN_D1)
-    const BCLK: PwmPin;
-    //% fixedInstance shim=pxt::getPin(PIN_D9)
-    const LRCLK: PwmPin;
-
-    
-
+    //% fixedInstance shim=pxt::getPin(PIN_D4)
+    const SOUND: DigitalInOutPin;
+ 
     //% fixedInstance shim=pxt::getPin(PIN_NEOPIXEL)
     const NEOPIXEL: DigitalInOutPin;
-
-
 }
 
